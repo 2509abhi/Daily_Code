49c61064dfb857837d158c4044e52ec9f5fc8210
Abhishek Bansal <92670086+2509abhi@users.noreply.github.com>
2023-03-05 21:08:23 +0530

    all_codeopen

--- pandas_dataHandling.py
+++ pandas_dataHandling.py
@@ -1,10 +1,11 @@
-# df = pd.read_csv('C:\\Users\\Abhishek\\OneDrive\\Desktop\\college usar\\SampleData.csv')
 import pandas as pd
 import numpy as np
-import xlrd as xl
-import openpyxl
+# import xlrd as xl
+# import openpyxl
 df = pd.read_csv('C:\\Users\\Abhishek\\OneDrive\\Desktop\\college usar\\SampleData.csv',na_values=['-1'])
+df.day= pd.to_datetime(df.day, infer_datetime_format=True)
 print(df.day)
+print(type(df.day[0]))
 df = df.set_index('day')
 print(df)
 ndf = df.fillna({
@@ -15,6 +16,22 @@ ndf = df.fillna({
 print(ndf)
 fdf = df.fillna(method = 'ffill')
 print(fdf)
-bdf = df.fillna(method = 'bfill')
+bdf = df.fillna(method = 'ffill', limit=1)
 print(bdf)
+# new_df = df.interpolate(mathod = "pad")
+# print(new_df.day)
+dt = pd.date_range('2017-02-02','02-02-2018')
+idx = pd.DatetimeIndex(dt)
+print(df.reindex(idx))
 
+print(df)
+print(df.replace({
+    'temperature': 32,
+    'widespread': 7,
+    'event': 'NaN'
+},np.nan))
+print(df.replace({
+    '28': 32,
+    'widespread': 7,
+    'event': 'NaN'
+}))
